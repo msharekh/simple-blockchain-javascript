38c0cd27d9c5b5680186eec87362202765204c5d
Mishal Alsharekh <mishal.sharekh@gmail.com>
2021-05-08 13:56:48 +0300

    1

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+class Block{
+    constructor(index, timestamp, data, previous){
+        this.index=index;
+        this.timestamp=timestamp;
+        this.data=data;
+        this.previous=previous;
+        this.hash = '';
+    }
+
+    calculateHash(){
+
+    }
+}
